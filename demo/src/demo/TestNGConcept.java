33463a34147fdad520b936782428080c5071577f
SAGAR ALASKAR <SAGAR ALASKAR@DESKTOP-NL9CC06>
2021-08-08 10:53:43 +0530

    First commit

---
+++ demo/src/demo/TestNGConcept.java
@@ -0,0 +1,103 @@
+package demo;
+
+public class TestNGConcept {
+/*
+ * Executing multiple programs or java classes using XML file
+ * Groupin test cases 
+ * parallel test execution 
+ * 
+ *  Executing multiple programs or java classes using XML file
+ *  
+ *  Tags in XML
+ *  -------------------------------------
+ *  <suit name = "Suit Name">
+ * <test name = "Test Name">
+ * <classes>
+ * <class name = "package.Class1Name"/>
+ * </classes>
+ * </test>
+ * </suite>
+ * 
+ * 
+ * Create XML file
+ * ------------------------------
+ *> select java project/package-->right click-->new-->other-->enter testng ->select testngclass
+ * > Enter testNG and testNG class
+ * >Enter source and package names
+ * >Enter XML file Name
+ * 
+ * TestNg Annotations
+ * --------------------------
+ * @Test- The annotated method is a part of a test case
+ * 
+ * @BeforeMethod - The annotated method will be run before each Test method
+ * @AfterMethod - The annotated method will be run after each test Method
+ * 
+ * @BeforeClass - The annotated method will run before the first method in the current clas is
+ *                  invoked.
+ *  @AfterClass - The annotated method will be run after all the Test methods in the current 
+ *                have been run.
+ *  
+ *                                
+ * @BeforeTest - the annotated method will be run before any Test method belonginig to classes
+ *               inside tag is run 
+ * @AfterTest - the annotated method will be run after any Test method belonginig to classes
+ *               inside tag have run .
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ */
+}
